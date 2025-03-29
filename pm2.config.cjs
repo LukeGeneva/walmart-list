65097bd1e0af80b9d4b44a921eb18f3b904b362a
Luke Geneva <lukegeneva@gmail.com>
2025-03-29 10:59:20 -0500

    Adjust pm2 config

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -4,8 +4,8 @@ module.exports = {
       name: 'walmart-list',
       script: 'bun',
       args: 'run index.ts',
-      cwd: './walmart-list',
-      interpreter: 'none', // Tell PM2 not to use Node.js
+      cwd: '~/projects/walmart-list',
+      interpreter: 'none', 
       env: {
         NODE_ENV: 'production',
         PORT: 3004,
