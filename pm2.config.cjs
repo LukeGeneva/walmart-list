030a6229ca411fb690ecc87c2eb81a46726761c8
Luke Geneva <lukegeneva@gmail.com>
2025-03-28 23:19:54 -0500

    Fix deploy

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: 'walmart-list',
       script: 'bun',
       args: 'run index.ts',
-      cwd: path.join(os.homedir(), 'projects', 'walmart-list'),
+      cwd: './walmart-list',
       interpreter: 'none', // Tell PM2 not to use Node.js
       env: {
         NODE_ENV: 'production',
