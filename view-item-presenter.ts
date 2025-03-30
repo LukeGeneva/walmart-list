d5055815a824765b8d6d39acfe6f7641d33d2000
Luke Geneva <lukegeneva@gmail.com>
2025-03-30 01:09:24 -0500

    List view logic

--- view-item-presenter.ts
+++ view-item-presenter.ts
@@ -3,11 +3,8 @@ import { dispense } from 'nutrimatic-html-dispenser';
 import type { ViewListItemOutput } from './use-cases/ViewListItem';
 
 const ITEM_VIEW = path.join(import.meta.dir, 'views/item.html');
-const FINISHED_VIEW = path.join(import.meta.dir, 'views/finished.html');
 
 export function presentItem(data: ViewListItemOutput) {
-  if (!data.item) return dispense(FINISHED_VIEW, {});
-
   const { listId, quantity } = data.item;
   const prevHref = `/list/${listId}/${data.prevItemId}`;
   const nextHref = `/list/${listId}/${data.nextItemId}`;
